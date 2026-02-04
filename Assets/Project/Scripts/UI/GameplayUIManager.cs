beb2d1458d849fb082a8f6f8f9b332326c1d3b46
Math-Maths <47351891+Math-Maths@users.noreply.github.com>
2026-02-03 21:22:24 -0300

    feature: created a UI Manager script for gameplay

--- Assets/Project/Scripts/UI/GameplayUIManager.cs
+++ Assets/Project/Scripts/UI/GameplayUIManager.cs
@@ -1,9 +1,50 @@
+using ChessGame.Managers;
+using TMPro;
 using UnityEngine;
 
 namespace ChessGame.UI
 {
     public class GameplayUIManager : MonoBehaviour
     {
-        
+        [Header("End Screen References")]
+        [SerializeField] private GameObject gameOverScreen;
+        [SerializeField] private TMP_Text gameResult;
+        [SerializeField] private string playerWinText;
+        [SerializeField] private string enemyWinText;
+        [SerializeField] private string drawText;
+
+        public void Initialize()
+        {
+            EventManager.Instance.AddListener<PieceSide>(EventNameSaver.OnKingCapture, ShowGameOverScreen);
+        }
+
+        private void OnDisable()
+        {
+            EventManager.Instance.RemoveListener<PieceSide>(EventNameSaver.OnKingCapture, ShowGameOverScreen);
+        }
+
+        private void ShowGameOverScreen(PieceSide winner)
+        {
+            EventManager.Instance.Invoke(EventNameSaver.OnGameOver);
+
+            if(winner == PieceSide.Player)
+                gameResult.text = playerWinText;
+            else if(winner == PieceSide.Enemy)
+                gameResult.text = enemyWinText;
+            else
+                gameResult.text = drawText;
+
+            gameOverScreen.SetActive(true);
+        }
+
+        public void ResetGame()
+        {
+            Debug.Log("Reset");
+        }
+
+        public void GoToMenu()
+        {
+            Debug.Log("Going to menu");
+        }
     }
 }
--- Assets/Project/Scripts/Util/EventNameSaver.cs
+++ Assets/Project/Scripts/Util/EventNameSaver.cs
@@ -1,4 +1,7 @@
 public static class EventNameSaver
 {
     public const string OnTurnChange = "OnTurnChange";
+    public const string OnStateChange = "OnStateChange";
+    public const string OnGameOver = "OnGameOver";
+    public const string OnKingCapture = "OnKingCapture";
 } 
